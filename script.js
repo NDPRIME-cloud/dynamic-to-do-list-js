72364a917685baeb2681b3a57dd60fae1ebf9932
ndprime <nikechideradivine@gmail.com>
2025-08-05 09:05:51 +0100

    done

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function(){
+const addButton = document.getElementById("add-task-btn")
+const taskInput = document.getElementById("task-input")
+const taskList = document.getElementById("task-list")
+
+function addTask() {
+    let taskText = taskInput.value.trim()
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+
+    }
+    
+        const li =document.createElement("li");
+        li.textContent = taskText;
+        
+    
+    const removeBtn = document.createElement("button")//creating the remove button
+    removeBtn.textContent = "Remove"
+    removeBtn.className = "remove-btn"
+    removeBtn.onclick =  function (){
+        taskList.removeChild(li) 
+    };
+
+    li.appendChild(removeBtn);//adds remove button to <li>
+
+    taskList.appendChild(li); // adds the whole li to tasklist
+
+    taskInput.value = ""
+
+}
+
+addButton.addEventListener("click", addTask)
+taskInput.addEventListener("keypress", function(event){
+    if(event.key === "Enter"){
+        addTask();
+    }
+
+})
+});
+
+    
+
+
